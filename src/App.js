8a7c0173d5eb5f32ac73d61bce9eb3ece3de83a2
Deepak Yadav <deepak5282786@gmail.com>
2022-12-22 21:52:56 +0530

    Header component created working on Home Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi I am Going to make Amazon Clone 👍👍👍</h1>
+      <Header />
+      <Home />
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+
+function Header() {
+  return (
+    <div className="header">
+      <img
+        className="header_logo"
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        alt=""
+      />
+      <div className="header_search">
+        <input type="text" className="header_searchInput" />
+        <SearchIcon className="header_searchIcon" />
+      </div>
+      <div className="header_nav">
+        <div className="header_option">
+          <span className="header_optionLineOne">Hello Deepak</span>
+          <span className="header_optionLineTwo">Sign In</span>
+        </div>
+        <div className="header_option">
+          <span className="header_optionLineOne">Return</span>
+          <span className="header_optionLineTwo"> & Orders</span>
+        </div>
+        <div className="header_option">
+          <span className="header_optionLineOne">Your</span>
+          <span className="header_optionLineTwo">Prime</span>
+        </div>
+        <div className="header_optionBasket">
+          <ShoppingCartIcon />
+          <span className="header_optionLineTwo header_basketCount">0</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
